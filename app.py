0181bcc56e773ac2b99f33ce82482fdbc29f9b74
wonjunJang <nuriyooyoung@gmail.com>
2023-08-30 11:07:44 +0900

    modify flask version

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, request, render_template
-from env import EMAIL, PORT
+from env import EMAIL, PORT, SEND_PASSWORD
 from static.email import Send_Email, Cert_Code
 
 app = Flask(__name__, static_url_path='/static')
 
-
-@app.route('/', methods=['GET','POST'])
-def send(): 
-    try : 
-        for i in range(3):
-            Send_Email(test[i], Cert_Code()+str(" 인증번호를 다음과 같이 입력해주세요!"), "메일 인증번호 입니다!")
-            print(f"{test[i]} 에게 성공적으로 메일을 전송하였습니다!")
+@app.route('/',methods=['GET', 'POST'])
+def send_email():
+    if request.method == 'POST':
+        message_target = request.form['target'] #상대방
+        message_title = request.form['title'] #제목
+        message_context = request.form['msg'] #내용
+        message_send_password = request.form['password'] #전송비밀번호
+        if message_send_password == SEND_PASSWORD.PASSWORD :
+            Send_Email(message_target, message_context, message_title)
+            print("successfully sent email")
+    return render_template('email.html')
             
-    except Exception as e:
-        print(e)
-        return render_template('index.html')
-        
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=False, port = PORT.PORT_NUM)
